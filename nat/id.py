c8c46b0132fd78ad27056db37ed9275976e3c49a
christian-oreilly <christian.oreilly@epfl.ch>
2016-10-05 17:59:30 +0200

    Adding functionnality to get informations about a publication, given some DOI/PMID.

--- nat/id.py
+++ nat/id.py
@@ -4,18 +4,39 @@ __author__ = "Christian O'Reilly"
 
 import urllib
 from bs4 import BeautifulSoup
+from dateutil.parser import parse
+import re
 
+try:
+    from .utils import Id2FileName
+except SystemError:
+    from nat.utils import Id2FileName
 
-
-def getIDfromPMID(PMID):
+def getPMIDSoup(PMID):
     url = "http://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/?tool=neurocurator&email=christian.oreilly@epfl.ch&ids=" + PMID + "&format=json&versions=no"
+    return getSoup(url)
+ 
+def getDOISoup(DOI): 
+    url = "http://doi.org/api/handles/" + DOI
+    return getSoup(url)
+ 
+ 
+def getSoup(url):
     try:
         with urllib.request.urlopen(url) as response:
            html = response.read()
     except urllib.error.HTTPError:
         return None
-
-    soup = BeautifulSoup(html, "lxml")
+    
+    return BeautifulSoup(html, "lxml")    
+     
+ 
+ 
+def getIDfromPMID(PMID):
+    soup = getPMIDSoup(PMID)
+    if soup is None:
+        return None
+    
     jsonStr = eval(str(soup)[15:-19])
     if jsonStr["status"] == "ok":
         if "doi" in jsonStr["records"][0]:
@@ -27,71 +48,120 @@ def getIDfromPMID(PMID):
 
     
 
+
+
+def getInfoFromID(ID): 
+    if "PMID" in ID:
+        idKind, PMID = ID.split("_")
+        return getInfoFromPMID(PMID)
+    else:
+        DOI = Id2FileName(ID)        
+        return getInfoFromDOI(DOI)
+
+
+
+def getYear(dateStr):
+    if dateStr == "":
+        return ""
+    else:
+        try:
+            return str(parse(dateStr).year)
+        except ValueError:
+            return re.search(r'[12]\d{3}', dateStr).group(0)
+
+def getInfoFromPMID(PMID):
+    soup = getSoup("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id=" + PMID + "&retmode=json")
+    if soup is None:
+        return None
+
+    result = eval(str(soup)[15:-19])["result"]
+    if PMID not in result:
+        return None
+    
+    authors = "; ".join([author["name"] for author in result[PMID]["authors"] 
+                                        if author["authtype"] == "Author"])
+    retData = {"authors":authors,
+               "journal":result[PMID]['fulljournalname'],
+               "year":getYear(result[PMID]['pubdate']),    
+               "issue":result[PMID]['issue'], 
+               "volume":result[PMID]['volume'],
+               "title":result[PMID]['title']} 
+
+    return retData
+
+
+
+def getInfoFromDOI(DOI):
+    soup = getSoup("http://api.crossref.org/works/" + DOI)
+    if soup is None:
+        return None
+    soup = str(soup)[15:-18]       
+    soup = soup.replace("true", "True")
+    soup = soup.replace("false", "False")
+    soup = eval(soup)["message"]
+    
+    authors = "; ".join([author["family"] + ", " + author["given"] for author in soup["author"]])   
+    year    = soup["issued"]["date-parts"][0][0]
+    #try:
+    #    year    = soup["published-print"]["date-parts"][0][0]
+    #except KeyError:
+    #    print(list(soup.keys()))
+    #    print(list(soup["issued"]['date-parts']))
+    #    print(list(soup["published-online"]['date-parts']))
+    
+    retData = {"authors":authors,
+               "journal":soup["container-title"][0],
+               "year":year,    
+               "issue":soup["issue"], 
+               "volume":soup["volume"],
+               "title":soup["title"][0]}     
+
+    return retData 
+ 
+
+
+
 def checkID(ID): 
-	if "PMID" in ID:
-		return checkPMID(ID)
-	else:
-		return checkDOI(ID)
+    if "PMID" in ID:
+        return checkPMID(ID)
+    else:
+        return checkDOI(ID)
 
 
 def checkPMID(ID):
-	idKind, PMID = ID.split("_")
-	url = "http://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/?tool=neurocurator&email=christian.oreilly@epfl.ch&ids=" + PMID + "&format=json&versions=no"
-	try:
-		with urllib.request.urlopen(url) as response:
-		   html = response.read()
-	except urllib.error.HTTPError:
-		return False
-
-	soup = BeautifulSoup(html, "lxml")
+    idKind, PMID = ID.split("_")
+    soup = getPMIDSoup(PMID)
+    if soup is None:
+        return False
      
-	return eval(str(soup)[15:-19])["status"] == "ok"
+    return eval(str(soup)[15:-19])["status"] == "ok"
 
 
 
 def checkDOI(DOI):
-	# Using the rest API: http://www.doi.org/factsheets/DOIProxy.html#rest-api
+    DOI = Id2FileName(DOI)
+    soup = getDOISoup(DOI)
+    if soup is None:
+        return False
 
-
-	# Replacement characters according to http://www.doi.org/factsheets/DOIProxy.html#rest-api
-	# but "%" signs must be replaced first since they are involved in the other replaced characters.
-	if '%' in DOI:
-		DOI = DOI.replace('%', '%25')
-
-	replacementDict = {'"':'%22', '#':'%23', ' ':'%20', '?':'%3F', 
-		'<':'%3C', '>':'%3E', '{':'%7B', '}':'%7D', '^':'%5E', 
-		'[':'%5B', ']':'%5D', '`':'%60', '|':'%7C', '\\':'%5C', '+':'%2B'}
-
-	# When Zotero imports from pubmed, it uses the "&lt;" for "<" and "&gt;" for ">"
-	# so we also replace these.
-	replacementDict["&lt;"] = '%3C'
-	replacementDict["&gt;"] = '%3E'
-
-	for key, value in replacementDict.items():
-		if key in DOI:
-			DOI = DOI.replace(key, value)
-
-	url = "http://doi.org/api/handles/" + DOI
-	try:
-		with urllib.request.urlopen(url) as response:
-		   html = response.read()
-	except urllib.error.HTTPError:
-		return False
-
-	soup = BeautifulSoup(html, "lxml")
-	# For some reason the "soup" string sometime contrains 
-	# dictionnary entries with values that are equare to 
-	# 'true' instead of 'True' which cannot be processed
-	# by the python's eval function.
-	soup = str(soup)[15:-18].replace("true", "True")
-	return eval(soup)["responseCode"] == 1
+    # For some reason the "soup" string sometime contrains 
+    # dictionnary entries with values that are equare to 
+    # 'true' instead of 'True' which cannot be processed
+    # by the python's eval function.
+    soup = str(soup)[15:-18].replace("true", "True")
+    return eval(soup)["responseCode"] == 1
 
 
 
 
 if __name__ == "__main__":
-	print(checkPMID("PMID_3303249260")) # False
-	print(checkPMID("PMID_3309260")) # True
-	print(checkPMID("PMID_26601117")) # True
-	print(checkPMID("PMID_26584868")) # True
+    #print(checkPMID("PMID_3303249260")) # False
+    #print(checkPMID("PMID_3309260")) # True
+    #print(checkPMID("PMID_26601117")) # True
+    #print(checkPMID("PMID_26584868")) # True
 
+    getInfoFromID("PMID_3309260")
+    getInfoFromID("10.1126/science.1207502")
+    getInfoFromID("10.1371/journal.pone.0107780")
+    
+    
--- nat/utils.py
+++ nat/utils.py
@@ -23,14 +23,40 @@ encoders["forwardSlash"] = ("/", "%2F")
 encoders["colon"]        = (":", "%3A")
 encoders["greater"]      = (">", "%3E")
 encoders["smaller"]      = ("<", "%3C")
+encoders["doubleQuote"]  = ('"', "%22")
+encoders["hash"]         = ("#", "%23")
+encoders["space"]        = (" ", "%20")
+encoders["iterog"]       = ("?", "%3F")
+encoders["curlyStart"]   = ("{", "%7B")
+encoders["curlyEnd"]     = ("}", "%7D")
+encoders["hat"]          = ("^", "%5E")
+encoders["squareStart"]  = ("[", "%5B")
+encoders["squareEnd"]    = ("]", "%5D")
+encoders["grave"]        = ("`", "%60")
+encoders["bar"]          = ("|", "%7C")
+encoders["backSlash"]    = ("\\", "%5C")
+encoders["plus"]         = ("+", "%2B")
+
+# When Zotero imports from pubmed, it uses the "&lt;" for "<" and "&gt;" for ">"
+# so we also replace these.
+encoders["zotLT" ]       = ("&lt;", "%3C")
+encoders["zotGT" ]       = ("&gt;", "%3E")
+
 
 
 
 def Id2FileName(ID):
+    # Replacement characters according to http://www.doi.org/factsheets/DOIProxy.html#rest-api
+    # but "%" signs must be replaced first since they are involved in the other replaced characters.
+    if '%' in ID:
+        ID = ID.replace('%', '%25')    
+    
     for key, (symbol, replacement) in encoders.items():
         assert(not replacement in ID)
         ID = ID.replace(symbol, replacement)
     return ID
+
+
 
 def fileName2Id(fileName):
     for key, (symbol, replacement) in encoders.items():
