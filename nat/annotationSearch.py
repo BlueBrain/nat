14b9c627cbfa8d9897acdad7d02b58b8cbef1ea8
Christian O'Reilly <christian.oreilly@epfl.ch>
2017-08-15 16:50:06 +0200

    Moving Condition classes out of annotationSearch.py (to condition.py).

--- nat/annotationSearch.py
+++ nat/annotationSearch.py
@@ -8,12 +8,16 @@ import os
 from glob import glob
 import pandas as pd
 import numpy as np
+import os.path
+import pickle
 
 from .annotation import Annotation
 from .modelingParameter import NumericalVariable, getParameterTypeNameFromID, Variable
 from .treeData import flatten_list
 from .ontoManager import OntoManager
 from .tagUtilities import nlx2ks    
+from .condition import Condition, ConditionAtom
+from .equivalenceFinder import EquivalenceFinder
 
 annotationKeys         = ["Annotation type", "Annotation ID", "Publication ID", "Has parameter", "Tag name", "Author"]
 annotationResultFields = ["Annotation type", "Publication ID", "Nb. parameters", "Tag name", "Comment", "Authors", "Localizer"]
@@ -23,200 +27,10 @@ parameterKeys          = ["Parameter name", "Result type", "Parameter instance I
                           "Publication ID", "Tag name"]
 parameterResultFields  = ["Required tag names", "Result type", "Values", "Parameter name", 
                           "Parameter type ID", "Parameter instance ID", "Unit", 
-                          "Text", "Context", "Species", "AgeCategory"] 
-
-
-class Condition:
-
-    def apply_param(self, parameters):
-        return parameters
-        
-
-    def apply_annot(self, annotations):
-        return annotations
-        
+                          "Text", "Context", "Species", "AgeCategories"] 
 
 
 
-
-
-def checkAnnotation(annotation, key, value):
-
-    if key == "Annotation type":
-        return annotation.type == value  
-        
-    elif key == "Publication ID":        
-        return annotation.pubId == value    
-        
-    elif key == "Annotation ID":        
-        return annotation.ID == value    
-        
-    elif key == "Has parameter":        
-        return (len(annotation.parameters) > 0) == bool(value)   
-        
-    elif key == "Tag name":        
-        print(value, annotation.tags)
-        for tag in annotation.tags:
-            if tag.name == value:
-                return True
-        return False        
-        
-    elif key == "Author":        
-        for author in annotation.authors:
-            if author == value:
-                return True
-        return False        
-        
-    else:
-        raise ValueError("Parameter key '" + str(key) + "' is not available for search.")
-
-
-
-
-
-
-
-def checkParameter(parameter, annotation, key, value):
-
-    if key == "Parameter name":
-        return getParameterTypeNameFromID(parameter.description.depVar.typeId) == value  
-        
-    elif key == "Parameter instance ID":        
-        return parameter.id == value
-        
-    elif key == "Result type":        
-        return parameter.description.type == value        
-        
-    elif key == "Unit":
-        if isinstance(parameter.description.depVar, Variable):
-            return parameter.description.depVar.unit == value
-        elif isinstance(parameter.description.depVar, NumericalVariable):
-            return parameter.description.depVar.values.textUnit() == value
-        else:
-            raise TypeError
-            
-    elif key == "Required tag name":
-        for tag in parameter.requiredTags:
-            if tag.name == value:
-                return True
-        return False
-        
-    elif key == "Annotation ID":
-        return annotation.annotId == value        
-
-        
-    elif key == "Publication ID":
-        return annotation.pubId == value      
-        
-    elif key == "Tag name":
-        for tag in annotation.tags:
-            if tag.name == value:
-                return True
-        return False
-        
-    elif key == "Keyword":        
-        raise NotImplemented
-    else:
-        raise ValueError("Parameter key '" + str(key) + "' is not available for search.")
-
-
-
-class ConditionAtom(Condition):
-    def __init__(self, key, value):
-        if not isinstance(key, str):
-            raise TypeError
-        if not isinstance(value, str):
-            raise TypeError
-            
-        self.key   = key
-        self.value = value
-
-    def apply_param(self, parameters):
-        return {param:annot for param, annot in parameters.items() 
-                            if checkParameter(param, annot, self.key, self.value)}
-        
-    def apply_annot(self, annotations):
-        return [annot for annot in annotations 
-                            if checkAnnotation(annot, self.key, self.value)]
-
-
-class ConditionAND(Condition):
-
-    def __init__(self, conditions):
-        if not isinstance(conditions, list):
-            raise TypeError
-        for condition in conditions:
-            if not isinstance(condition, Condition):
-                raise TypeError
-        self.conditions = conditions
-
-    def apply_param(self, parameters):
-        for condition in self.conditions:
-            parameters = condition.apply_param(parameters)
-        return parameters      
-        
-        
-        
-    def apply_annot(self, annotations):
-        for condition in self.conditions:
-            annotations = condition.apply_annot(annotations)
-        return annotations      
-        
-            
-    
-class ConditionOR(Condition):
-    
-    def __init__(self, conditions):
-        if not isinstance(conditions, list):
-            raise TypeError
-        for condition in conditions:
-            if not isinstance(condition, Condition):
-                raise TypeError
-        self.conditions = conditions
-
-    def apply_param(self, parameters):
-        paramOut = {}
-        for condition in self.conditions:
-            parameters = condition.apply_param(parameters)
-            paramOut.update(parameters)
-        return paramOut              
-        
-        
-    def apply_annot(self, annotations):
-        annotOut = []
-        for condition in self.conditions:
-            annotations = condition.apply_annot(annotations)
-            for annot in annotations:
-                if not annot in annotOut:
-                    annotOut.append(annot)
-        return annotOut       
-                
-        
-class ConditionNOT(Condition):
-
-    def __init__(self, condition):
-        if not isinstance(condition, Condition):
-            raise TypeError
-        self.condition = condition
-
-
-    def apply_param(self, parameters):
-        paramToRemove = self.condition.apply_param(parameters)
-        for key in paramToRemove:
-            del parameters[key]
-        return parameters        
-        
-        
-    def apply_annot(self, annotations):
-        annotToRemove = self.condition.apply_annot(annotations)
-        for annot in annotToRemove:
-            annotations.remove(annot)
-        return annotations      
-                
-
-
-import os.path
-import pickle
 class CompiledCorpus:
     
     def __init__(self, binPath="annotations.bin"):
@@ -269,6 +83,7 @@ class Search:
         self.pathDB     = pathDB 
         self.getAllAnnotations()
         self.selectedItems = None
+        self.findEquivalences = True
 
     def searchAnnotations(self):
         pass
@@ -349,6 +164,8 @@ class AnnotationSearch(Search):
         self.resultFields = annotationResultFields
 
     def search(self):
+        if self.findEquivalences:
+            self.conditions = EquivalenceFinder(self.conditions)
         self.selectedItems = self.conditions.apply_annot(self.annotations)
         resultDF           = self.formatOutput(self.selectedItems)
         return resultDF
@@ -404,6 +221,8 @@ class ParameterSearch(Search):
 
 
     def search(self):
+        if self.findEquivalences:
+            self.conditions = EquivalenceFinder(self.conditions)        
         self.selectedItems = self.conditions.apply_param(self.parameters)
         resultDF           = self.formatOutput(self.selectedItems)
         return resultDF
@@ -482,8 +301,8 @@ class ParameterSearch(Search):
             elif field == "Species":
                 results[field] = [annot.getSpecies() for annot in annotations]  
                 
-            elif field == "AgeCategory":
-                results[field] = [annot.getAge() for annot in annotations]     
+            elif field == "AgeCategories":
+                results[field] = [annot.getAgeCategories() for annot in annotations]     
 
             elif field == "Values":
                 if self.onlyCentralTendancy:
