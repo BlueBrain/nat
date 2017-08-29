ef078dfa30e344c951315913660d8107d8e82432
Christian O'Reilly <christian.oreilly@epfl.ch>
2017-08-29 17:12:42 +0200

    Lot of modifications to paramSample.py to provid traceability of the modifications to the sample.

--- nat/paramSample.py
+++ nat/paramSample.py
@@ -5,9 +5,9 @@ Created on Tue Aug 15 17:14:30 2017
 @author: oreilly
 """
 
-from warnings import warn
 from quantities import Quantity
 import numpy as np
+from copy import copy
 
 from .variable import NumericalVariable
 from .values import ValuesSimple, ValuesCompound
@@ -16,6 +16,7 @@ from .modelingParameter import getParameterTypeIDFromName, getParameterTypeNameF
 from .annotationSearch import ParameterGetter
 from .zoteroWrap import ZoteroWrap
 from .ageResolver import AgeResolver
+from .treeData import getChildren
 
 
 class ParamSample:
@@ -38,6 +39,20 @@ class ParamSample:
         # setting this parametes to min, 5 months will be attributed as numerical
         # value for the age "adult" in rats.
         self.ageTypeValue = "min"
+        
+        self.sampleDF["isValid"] = None
+        self.sampleDF["statusStr"] = ""
+        self.__report = "Search string: " + str(self.searcher.conditions) + "\n"
+        if self.searcher.onlyCentralTendancy:
+            self.__report = "Search : Using central tendencies for single annotations containing multiple values.\n"
+        self.__aggregators = {}
+            
+
+
+    def copy(self):
+        copiedSample = copy(self)            
+        copiedSample.sampleDF = self.sampleDF.copy()
+        return copiedSample
             
     def setZoteroLib(self, libraryId, libraryType, apiKey):
         self.zotWrap = ZoteroWrap()
@@ -52,9 +67,12 @@ class ParamSample:
             thickness = Quantity(thicknessValue, thicknessUnit)
             return (density/thickness).rescale(desiredUnit)        
         
+
+        self.__report += "Rescaling the units to '" + str(unit) + "'.\n"   
+        if rescaleStereo:
+            self.__report += "Rescaling densities from 2D densities to 3D.\n"              
         
         paramGetter = ParameterGetter(pathDB=self.searcher.pathDB)
-        rowsToDrop = []
         for param, annot, (index, row) in zip(self.sampleDF["obj_parameter"], 
                                               self.sampleDF["obj_annotation"], 
                                               self.sampleDF.iterrows()):
@@ -81,24 +99,21 @@ class ParamSample:
                             self.sampleDF.loc[index, "Unit"]          = param.unit                   
                             continue
                 
-                warn("The annotation with the parameter ID " + row["Parameter instance ID"] + 
-                     " cannot be rescaled from unit " + 
-                     str(param.unit) + " to unit " + str(unit) + ". Dropping this record.")
-                rowsToDrop.append(index)
+                statusStr = "Cannot be rescaled to unit " + str(unit) + "\n"
+                self.sampleDF.loc[index, "isValid"]   = False
+                self.sampleDF.loc[index, "statusStr"] += statusStr
                 continue                
 
             if Quantity(1, param.unit) != Quantity(1, unit):
-                warn("The annotation with the parameter ID " + row["Parameter instance ID"] + 
-                     " cannot be rescaled from unit " + 
-                     str(param.unit) + " to unit " + str(unit) + ". Dropping this record.")
-                rowsToDrop.append(index)
+                statusStr = "Cannot be rescaled to unit " + str(unit) + "\n"
+                self.sampleDF.loc[index, "isValid"]   = False
+                self.sampleDF.loc[index, "statusStr"] += statusStr
                 continue                            
                 
             self.sampleDF.loc[index, "obj_parameter"] = param   
             self.sampleDF.loc[index, "Values"]        = param.valuesText()   
             self.sampleDF.loc[index, "Unit"]          = param.unit   
 
-        self.sampleDF.drop(rowsToDrop, inplace=True)
 
 
 
@@ -111,23 +126,28 @@ class ParamSample:
                                     + "passed to ParamSample.reformatAsNumericalTraces() are incompatible.")
             else:
                 indepVarId = getParameterTypeIDFromName(indepVarName)
+                
+            self.__report += "Reformating as numerical traces with independant variable '" \
+                             + indepVarName + "'.\n"                      
+                
         else:
             if indepVarId is None:
                 raise ValueError("At least one of the attribute indepVarName and indepVarId "
                                     + "passed to ParamSample.reformatAsNumericalTraces() most not be None.")
             indepVarName = getParameterTypeNameFromID(indepVarId)
                 
+            self.__report += "Reformating as numerical traces with independant variable ID '" \
+                             + indepVarId + "'.\n"                        
 
-        rowsToDrop = []
+                
         for index, row in self.sampleDF.iterrows():
         
             if row["Result type"] == "pointValue":
                 indepVar = row[indepVarName]
                 if indepVar is None:
-                    warn("The annotation with the parameter ID " + row["Parameter instance ID"] + 
-                         " cannot be transformed to a numerical trace because no value has been attributed"+ 
-                         " ot the corresponding independant variable. Dropping this record.")
-                    rowsToDrop.append(index)
+                    statusStr = "Cannot be transformed to a numerical trace: no values for the independant variable.\n"
+                    self.sampleDF.loc[index, "isValid"]   = False
+                    self.sampleDF.loc[index, "statusStr"] += statusStr
                     continue                   
                                 
                 # Building the independant variables   
@@ -169,8 +189,6 @@ class ParamSample:
                 # Building the numerical trace parameter
                 row["obj_parameter"].description = ParamDescTrace(depVar, [indepVar])
                 row["Result type"] = "numericalTrace"
-                
-        self.sampleDF.drop(rowsToDrop, inplace=True)
 
 
         
@@ -182,16 +200,12 @@ class ParamSample:
     def preprocess_species(self):
         speciesId = []
         species   = []
-        rowsToDrop = []
         for index, row in self.sampleDF.iterrows():
             tags =row["Species"]
-            if len(tags) > 1 :
-                warn("The annotation with the parameter ID " + row["Parameter instance ID"] + 
-                     " is associated with more than one species (" + 
-                     str([tag.name for tag in tags]) 
-                     + "). The species cannot be automatically attributed unambiguously. " +
-                     "Skipping this record.")
-                rowsToDrop.append(index)
+            if len(tags) > 1 :                
+                statusStr = "Species ambiguous. More than one species associated to the annotation.\n"
+                self.sampleDF.loc[index, "isValid"]   = False
+                self.sampleDF.loc[index, "statusStr"] += statusStr                
                 continue
                 
             speciesId.append(tags[0].id)
@@ -199,7 +213,8 @@ class ParamSample:
             
         self.sampleDF["SpeciesId"] = speciesId
         self.sampleDF["Species"]   = species
-        self.sampleDF.drop(rowsToDrop, inplace=True)
+        
+        self.__report += "Extracting species from annotations.\n"                  
 
 
     def preprocess_age(self):    
@@ -209,17 +224,14 @@ class ParamSample:
         ageCategoryIds = []
         ageCategories  = []
         numericalAges  = []
-        rowsToDrop = []
         for index, row in self.sampleDF.iterrows():
             
             # First check if an experimental property with age as been attributed to the record            
             ageExpProp = [expProp.instanceId for expProp in row["obj_annotation"].experimentProperties if expProp.paramTypeId == 'BBP-002001']
             if len(ageExpProp) > 1 :
-                warn("The annotation with the parameter ID " + row["Parameter instance ID"] + 
-                     " is associated with more than one species age experimental properties." + 
-                     +" The age cannot be automatically attributed unambiguously. " +
-                     "Skipping this record.")
-                rowsToDrop.append(index)
+                statusStr = "Age is ambiguous. More than one age experimentation property is associated with the annotation.\n"
+                self.sampleDF.loc[index, "isValid"]   = False
+                self.sampleDF.loc[index, "statusStr"] += statusStr                
                 continue   
             
             if len(ageExpProp) == 1 :
@@ -235,13 +247,9 @@ class ParamSample:
             else:
                 tags = row["AgeCategories"]
                 if len(tags) > 1 :
-                    warn("The annotation with the parameter ID " + row["Parameter instance ID"] + 
-                         " is associated with more than one age categories (" + 
-                         str([tag.name for tag in tags]) 
-                         + "). The age cannot be automatically attributed unambiguously. " +
-                         "Skipping this record.")
-                    rowsToDrop.append(index)
-                    continue  
+                    statusStr = "Age is ambiguous. More than one age category is associated with the annotation.\n"
+                    self.sampleDF.loc[index, "isValid"]   = False
+                    self.sampleDF.loc[index, "statusStr"] += statusStr                               
                     
                 if len(tags) == 0:
                     ageCategoryIds.append(None)
@@ -258,7 +266,11 @@ class ParamSample:
         self.sampleDF["AgeCategoryId"] = ageCategoryIds
         self.sampleDF["AgeCategory"]   = ageCategories
         self.sampleDF["age"]           = numericalAges
-        self.sampleDF.drop(rowsToDrop, inplace=True)
+
+        
+        self.__report += "Preprocessing age information.\n"                  
+
+
 
     
     def preprocess_ref(self):    
@@ -269,3 +281,115 @@ class ParamSample:
         self.sampleDF["ref"] = [self.zotWrap.getInTextCitationFromID(annot.pubId) 
                                            for annot in self.sampleDF["obj_annotation"]]
     
+        self.__report += "Preprocessing associated references.\n"            
+    
+        
+    def filter_species(self, speciesTermId):
+        self.rootSpeciesId = speciesTermId
+        childrenDict = getChildren(speciesTermId)
+
+        if not "SpeciesId" in self.sampleDF:
+            self.preprocess_species()
+            
+        for index, row in self.sampleDF.iterrows():
+
+            if not row["SpeciesId"] in childrenDict:
+                statusStr = "Species filtered out. Not a children of " + speciesTermId + ".\n"
+                row["isValid"]   = False
+                row["statusStr"] += statusStr                
+
+        self.__report += "Filter species using the species ID '" + speciesTermId + "'.\n"            
+
+
+        
+    def validateUndefined(self):
+        self.sampleDF.loc[np.equal(self.sampleDF["isValid"], None), "isValid"] = True
+
+        self.__report += "Validated parameters with undefined status.\n"     
+
+
+    def addAggregator(self, aggregator):
+        self.__aggregators[aggregator.paramName] = aggregator
+        self.__report += "Adding an aggregator for parameter '" + aggregator.paramName + \
+                         "':" + str(aggregator) + ".\n"            
+        
+    def getParamValues(self, paramName=None, paramId=None, useOnlyValids=True):
+        """
+        Return the rows of sampleDF that are associated to the parameter
+        specified in paramName.
+        """
+        
+        if not paramName is None:
+            if not paramId is None:
+                if getParameterTypeNameFromID(paramId) != paramName:
+                    raise ValueError("Parameters paramId and paramName "
+                                    + "passed to ParamSample.getParamValues() are incompatible.")
+        else:
+            if paramId is None:
+                raise ValueError("At least one of the attribute paramName and paramId "
+                                    + "passed to ParamSample.getParamValues() most not be None.")
+            paramName = getParameterTypeNameFromID(paramId)
+        
+        df = self.sampleDF
+        if useOnlyValids:
+            df = df[df["isValid"] == True]
+
+        df.loc[:, "paramNames"] = [getParameterTypeNameFromID(param.typeId) for param in df["obj_parameter"]]
+
+        return df[df["paramNames"] == paramName]            
+        
+
+
+
+
+    def interpolate(self, interpValues):
+        """
+        interpValues should be a dictionnary where the keys are the parameter names
+        for the independant variable for which interpolation should be run and
+        the values are the value to which the parameter should be interpolated.
+        """        
+        
+        df = self.sampleDF
+        self.interpValues = interpValues
+        for interParamName, value  in interpValues.items():
+            self.__report += "Interpolation of the parameters for independent variables '" \
+                             + interParamName + "' at value " + str(value) + ".\n"   
+            finalValues = np.zeros((len(df["Result type"])))
+            for ind, (paramTrace, resType) in enumerate(zip(df["obj_parameter"], df["Result type"])):
+                if resType == "numericalTrace" and interParamName in paramTrace.indepNames:
+                    val = paramTrace.getInterp1dValues(value, interParamName, statsToReturn=["mean"])
+                    if isinstance(val, list):
+                        if len(val) == 1:
+                            val = val[0]
+                        else:
+                            raise ValueError("This case has not been implemented yet.")
+                    df.loc[ind, "Values"] = float(val)   
+
+                    
+
+    
+
+
+
+
+    def getModelingValues(self, paramName):
+        """
+        Return the values to use in modeling for the parameterId, by the 
+        aggregation method specified by the associated aggregator.
+        """        
+        
+        return self.__aggregators[paramName].values(self)
+
+    @property 
+    def validSample(self):
+        return self.sampleDF.loc[self.sampleDF["isValid"]]
+        
+    @property 
+    def invalidSample(self):
+        return self.sampleDF.loc[np.logical_not(self.sampleDF["isValid"])]
+
+    @property 
+    def report(self):
+        return self.__report
+
+    #def get
