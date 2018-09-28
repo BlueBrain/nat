6c7e2216a32191ea8344100094559d1bd3a6ef44
Pierre-Alexandre Fonta <pierre-alexandre.fonta@epfl.ch>
2018-09-28 17:29:42 +0200

    WIP setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 from setuptools import setup
 
-VERSION = "0.4.3"
+VERSION = "0.4.4"
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
@@ -16,11 +16,12 @@ with open(os.path.join(HERE, "README.md"), encoding="utf-8") as f:
 setup(
     name="nat-server",
     version=VERSION,
+    # FIXME
     description="Module to pawn the NAT REST server.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     keywords="neuroscience annotation curation literature modeling parameters",
-    url="https://github.com/BlueBrain/nat/tree/nat_server",
+    url="https://github.com/BlueBrain/nat/tree/nat-server",
     author="Christian O'Reilly, Pierre-Alexandre Fonta",
     author_email="christian.oreilly@epfl.ch, pierre-alexandre.fonta@epfl.ch",
     # NB: 'If maintainer is provided, distutils lists it as the author in PKG-INFO'.
@@ -29,15 +30,14 @@ setup(
     # maintainer_email="pierre-alexandre@epfl.ch",
     license="GPLv3",
     packages=["nat-server"],
-    python_requires=">=3",  # unittest.mock needs Python 3.3+.
+    # FIXME
+    python_requires=">=3.4",
+    # FIXME
     install_requires=[
         "nat",
         "flask",
         "PyPDF2"
     ],
-    extras_require={
-        "test": ["pytest", "pytest-cov", "pytest-lazy-fixture", "pytest-mock"]
-    },
     data_files=[("", ["LICENSE.txt"])],
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -47,6 +47,7 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "License :: Free for non-commercial use",
+        # FIXME
         "Programming Language :: Python :: 3 :: Only",
         "Operating System :: MacOS",
         "Operating System :: POSIX :: Linux",
